d8ddf7190bb2d1355ad72958820c00e103452a9a
Jan Johannes <jan@ntr.io>
2016-10-04 14:23:35 +0200

    fix: show edit box on new tabs

--- js/components/navigationBar.js
+++ js/components/navigationBar.js
@@ -34,6 +34,7 @@ class NavigationBar extends ImmutableComponent {
   get titleMode () {
     return this.props.mouseInTitlebar === false &&
       this.props.title &&
+      !['about:blank', 'about:newtab'].includes(this.props.location) &&
       !this.props.navbar.getIn(['urlbar', 'focused']) &&
       !this.props.navbar.getIn(['urlbar', 'active']) &&
       getSetting(settings.DISABLE_TITLE_MODE) === false
